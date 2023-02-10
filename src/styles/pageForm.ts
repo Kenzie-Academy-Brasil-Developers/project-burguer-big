a50e86795e64f87e7323c1782994baf4529a8991
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-02-10 14:59:43 -0300

    fix: margin form

--- src/styles/pageForm.ts
+++ src/styles/pageForm.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledSection = styled.section`
   width: 90%;
   padding: 10px;
-  margin: 30px auto 0;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: left;
